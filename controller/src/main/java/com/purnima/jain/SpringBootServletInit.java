c007836ef8fc86b4f80e262f16912cfc018c1417
michael <moktc@hotmail.com>
2021-05-14 09:39:19 +0800

    Add lines to run as war

---
+++ controller/src/main/java/com/purnima/jain/SpringBootServletInit.java
@@ -0,0 +1,13 @@
+package com.purnima.jain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication(scanBasePackages = "com.purnima.jain.customer")
+
+/**
+ * Make sure to include the modules into the dependencies section in the controller pom.xml
+ */
+@SpringBootApplication
+public class SpringBootServletInit extends SpringBootServletInitializer {
+}
