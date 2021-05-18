bf855513c6ee45ef8675603c1f553b97f851010e
michael <moktc@hotmail.com>
2021-05-18 23:35:22 +0800

    Add environment variable lookup

--- application/src/main/java/com/purnima/jain/customer/CustomerApplication.java
+++ application/src/main/java/com/purnima/jain/customer/CustomerApplication.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class CustomerApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(CustomerApplication.class);
--- controller/src/main/java/com/purnima/jain/customer/controller/CustomerController.java
+++ controller/src/main/java/com/purnima/jain/customer/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.purnima.jain.customer.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,22 @@ public class CustomerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
+	@Value("${siteName}")
+	private String siteName;
+
 	@Autowired
 	private CustomerService customerService;
 
+	/**
+	 * use this URL to test
+	 * http://localhost:8080/controller_war_exploded/customer/1
+	 *
+	 * @param customerId
+	 * @return
+	 */
 	@GetMapping("customer/{customerId}")
 	public Customer getCustomer(@PathVariable Integer customerId) {
-		logger.info("Inside CustomerController........");
+		logger.info("Inside CustomerController........ siteName " + siteName);
 		return customerService.getCustomerById(customerId);
 	}
 
